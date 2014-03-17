8e95069fcf7ab68af590c8e5ac57c1b2072c9100
jiangyao <jiangyao@rongyitech.com>
2014-03-17 10:28:12 +0800

    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProSx::Application.config.secret_key_base = 'a8e5e770c956b546b3ec5d99d5aece4acb7d49fd26af4954c425dc9784b4468360adbc0cd336ba55bc3888ce93fe1a71a8dbb6c0ac2dbfa27832ac1ba28f7b7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProSx::Application.config.session_store :encrypted_cookie_store, key: '_pro_sx_session'
